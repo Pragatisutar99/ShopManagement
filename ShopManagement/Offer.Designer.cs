ad222023f9953d09e70b28b884e58f1b7dd9e86d
Pragatisutar99 <ps3277679@gmail.com>
2023-09-01 13:39:30 +0530

    initial

---
+++ ShopManagement/Offer.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ShopManagement
+{
+    partial class Offer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOfferForm = new System.Windows.Forms.Label();
+            this.lblOfferinformation = new System.Windows.Forms.Label();
+            this.lblOriginalPriceOf = new System.Windows.Forms.Label();
+            this.lblDiscountinPercentage = new System.Windows.Forms.Label();
+            this.lblNewPrice = new System.Windows.Forms.Label();
+            this.btnUpdatePrice = new System.Windows.Forms.Button();
+            this.btnClaculateNewPrice = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDiscountinPercentage = new System.Windows.Forms.TextBox();
+            this.txtNewPrice = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblOfferForm
+            // 
+            this.lblOfferForm.AutoSize = true;
+            this.lblOfferForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOfferForm.Location = new System.Drawing.Point(536, 34);
+            this.lblOfferForm.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOfferForm.Name = "lblOfferForm";
+            this.lblOfferForm.Size = new System.Drawing.Size(115, 25);
+            this.lblOfferForm.TabIndex = 0;
+            this.lblOfferForm.Text = "Offer Form";
+            // 
+            // lblOfferinformation
+            // 
+            this.lblOfferinformation.AutoSize = true;
+            this.lblOfferinformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOfferinformation.Location = new System.Drawing.Point(190, 126);
+            this.lblOfferinformation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOfferinformation.Name = "lblOfferinformation";
+            this.lblOfferinformation.Size = new System.Drawing.Size(173, 25);
+            this.lblOfferinformation.TabIndex = 1;
+            this.lblOfferinformation.Text = "Offer Information";
+            // 
+            // lblOriginalPriceOf
+            // 
+            this.lblOriginalPriceOf.AutoSize = true;
+            this.lblOriginalPriceOf.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOriginalPriceOf.Location = new System.Drawing.Point(190, 220);
+            this.lblOriginalPriceOf.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOriginalPriceOf.Name = "lblOriginalPriceOf";
+            this.lblOriginalPriceOf.Size = new System.Drawing.Size(172, 25);
+            this.lblOriginalPriceOf.TabIndex = 2;
+            this.lblOriginalPriceOf.Text = "Original Price of ";
+            this.lblOriginalPriceOf.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblDiscountinPercentage
+            // 
+            this.lblDiscountinPercentage.AutoSize = true;
+            this.lblDiscountinPercentage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiscountinPercentage.Location = new System.Drawing.Point(190, 314);
+            this.lblDiscountinPercentage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDiscountinPercentage.Name = "lblDiscountinPercentage";
+            this.lblDiscountinPercentage.Size = new System.Drawing.Size(235, 25);
+            this.lblDiscountinPercentage.TabIndex = 3;
+            this.lblDiscountinPercentage.Text = "Discount in Percentage";
+            // 
+            // lblNewPrice
+            // 
+            this.lblNewPrice.AutoSize = true;
+            this.lblNewPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewPrice.Location = new System.Drawing.Point(190, 431);
+            this.lblNewPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNewPrice.Name = "lblNewPrice";
+            this.lblNewPrice.Size = new System.Drawing.Size(109, 25);
+            this.lblNewPrice.TabIndex = 4;
+            this.lblNewPrice.Text = "New Price";
+            // 
+            // btnUpdatePrice
+            // 
+            this.btnUpdatePrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdatePrice.Location = new System.Drawing.Point(312, 568);
+            this.btnUpdatePrice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUpdatePrice.Name = "btnUpdatePrice";
+            this.btnUpdatePrice.Size = new System.Drawing.Size(152, 35);
+            this.btnUpdatePrice.TabIndex = 5;
+            this.btnUpdatePrice.Text = "Updated Price";
+            this.btnUpdatePrice.UseVisualStyleBackColor = true;
+            this.btnUpdatePrice.Click += new System.EventHandler(this.btnUpdatePrice_Click);
+            // 
+            // btnClaculateNewPrice
+            // 
+            this.btnClaculateNewPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClaculateNewPrice.Location = new System.Drawing.Point(669, 568);
+            this.btnClaculateNewPrice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClaculateNewPrice.Name = "btnClaculateNewPrice";
+            this.btnClaculateNewPrice.Size = new System.Drawing.Size(158, 35);
+            this.btnClaculateNewPrice.TabIndex = 6;
+            this.btnClaculateNewPrice.Text = "Calculate New Price";
+            this.btnClaculateNewPrice.UseVisualStyleBackColor = true;
+            this.btnClaculateNewPrice.Click += new System.EventHandler(this.btnClaculateNewPrice_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(550, 220);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(711, 220);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 25);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // txtDiscountinPercentage
+            // 
+            this.txtDiscountinPercentage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiscountinPercentage.Location = new System.Drawing.Point(540, 314);
+            this.txtDiscountinPercentage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDiscountinPercentage.Name = "txtDiscountinPercentage";
+            this.txtDiscountinPercentage.Size = new System.Drawing.Size(148, 30);
+            this.txtDiscountinPercentage.TabIndex = 9;
+            // 
+            // txtNewPrice
+            // 
+            this.txtNewPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNewPrice.Location = new System.Drawing.Point(540, 420);
+            this.txtNewPrice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNewPrice.Name = "txtNewPrice";
+            this.txtNewPrice.Size = new System.Drawing.Size(148, 30);
+            this.txtNewPrice.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(734, 325);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 25);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "%";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(734, 431);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 25);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Rs";
+            // 
+            // Offer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 752);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtNewPrice);
+            this.Controls.Add(this.txtDiscountinPercentage);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnClaculateNewPrice);
+            this.Controls.Add(this.btnUpdatePrice);
+            this.Controls.Add(this.lblNewPrice);
+            this.Controls.Add(this.lblDiscountinPercentage);
+            this.Controls.Add(this.lblOriginalPriceOf);
+            this.Controls.Add(this.lblOfferinformation);
+            this.Controls.Add(this.lblOfferForm);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Offer";
+            this.Text = "Offer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOfferForm;
+        private System.Windows.Forms.Label lblOfferinformation;
+        private System.Windows.Forms.Label lblOriginalPriceOf;
+        private System.Windows.Forms.Label lblDiscountinPercentage;
+        private System.Windows.Forms.Label lblNewPrice;
+        private System.Windows.Forms.Button btnUpdatePrice;
+        private System.Windows.Forms.Button btnClaculateNewPrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDiscountinPercentage;
+        private System.Windows.Forms.TextBox txtNewPrice;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ ShopManagement/RegisterUser.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace ShopManagement
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRegisterUser = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblMobilenumber = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtMobileNumber = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.cmbbxType = new System.Windows.Forms.ComboBox();
+            this.cmbbxState = new System.Windows.Forms.ComboBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.rdbbtnMale = new System.Windows.Forms.RadioButton();
+            this.rdbbtnFemale = new System.Windows.Forms.RadioButton();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRegisterUser
+            // 
+            this.lblRegisterUser.AutoSize = true;
+            this.lblRegisterUser.Location = new System.Drawing.Point(387, 14);
+            this.lblRegisterUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRegisterUser.Name = "lblRegisterUser";
+            this.lblRegisterUser.Size = new System.Drawing.Size(107, 20);
+            this.lblRegisterUser.TabIndex = 0;
+            this.lblRegisterUser.Text = "Register User";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(189, 55);
+            this.lblType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(43, 20);
+            this.lblType.TabIndex = 1;
+            this.lblType.Text = "Type";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(189, 134);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(51, 20);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(189, 229);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(48, 20);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(189, 329);
+            this.lblGender.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(63, 20);
+            this.lblGender.TabIndex = 4;
+            this.lblGender.Text = "Gender";
+            this.lblGender.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(194, 415);
+            this.lblState.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(48, 20);
+            this.lblState.TabIndex = 5;
+            this.lblState.Text = "State";
+            // 
+            // lblMobilenumber
+            // 
+            this.lblMobilenumber.AutoSize = true;
+            this.lblMobilenumber.Location = new System.Drawing.Point(189, 523);
+            this.lblMobilenumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMobilenumber.Name = "lblMobilenumber";
+            this.lblMobilenumber.Size = new System.Drawing.Size(113, 20);
+            this.lblMobilenumber.TabIndex = 6;
+            this.lblMobilenumber.Text = "Mobile number";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(510, 129);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(180, 26);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(508, 218);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(180, 26);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // txtMobileNumber
+            // 
+            this.txtMobileNumber.Location = new System.Drawing.Point(510, 518);
+            this.txtMobileNumber.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMobileNumber.Name = "txtMobileNumber";
+            this.txtMobileNumber.Size = new System.Drawing.Size(180, 26);
+            this.txtMobileNumber.TabIndex = 9;
+            this.txtMobileNumber.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(510, 626);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(180, 26);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // cmbbxType
+            // 
+            this.cmbbxType.FormattingEnabled = true;
+            this.cmbbxType.Location = new System.Drawing.Point(510, 51);
+            this.cmbbxType.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbbxType.Name = "cmbbxType";
+            this.cmbbxType.Size = new System.Drawing.Size(180, 28);
+            this.cmbbxType.TabIndex = 11;
+            this.cmbbxType.SelectedIndexChanged += new System.EventHandler(this.cmbbxType_SelectedIndexChanged);
+            // 
+            // cmbbxState
+            // 
+            this.cmbbxState.FormattingEnabled = true;
+            this.cmbbxState.Location = new System.Drawing.Point(510, 403);
+            this.cmbbxState.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbbxState.Name = "cmbbxState";
+            this.cmbbxState.Size = new System.Drawing.Size(180, 28);
+            this.cmbbxState.TabIndex = 12;
+            this.cmbbxState.SelectedIndexChanged += new System.EventHandler(this.cmbbxState_SelectedIndexChanged);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(189, 637);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(78, 20);
+            this.lblPassword.TabIndex = 13;
+            this.lblPassword.Text = "Password";
+            // 
+            // rdbbtnMale
+            // 
+            this.rdbbtnMale.AutoSize = true;
+            this.rdbbtnMale.Location = new System.Drawing.Point(528, 320);
+            this.rdbbtnMale.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rdbbtnMale.Name = "rdbbtnMale";
+            this.rdbbtnMale.Size = new System.Drawing.Size(68, 24);
+            this.rdbbtnMale.TabIndex = 14;
+            this.rdbbtnMale.TabStop = true;
+            this.rdbbtnMale.Text = "Male";
+            this.rdbbtnMale.UseVisualStyleBackColor = true;
+            this.rdbbtnMale.CheckedChanged += new System.EventHandler(this.rdbMale_CheckedChanged);
+            // 
+            // rdbbtnFemale
+            // 
+            this.rdbbtnFemale.AutoSize = true;
+            this.rdbbtnFemale.Location = new System.Drawing.Point(720, 320);
+            this.rdbbtnFemale.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rdbbtnFemale.Name = "rdbbtnFemale";
+            this.rdbbtnFemale.Size = new System.Drawing.Size(87, 24);
+            this.rdbbtnFemale.TabIndex = 15;
+            this.rdbbtnFemale.TabStop = true;
+            this.rdbbtnFemale.Text = "Female";
+            this.rdbbtnFemale.UseVisualStyleBackColor = true;
+            this.rdbbtnFemale.CheckedChanged += new System.EventHandler(this.rbtFemale_CheckedChanged);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(392, 708);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(112, 35);
+            this.btnRegister.TabIndex = 16;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(658, 708);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 35);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 792);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.rdbbtnFemale);
+            this.Controls.Add(this.rdbbtnMale);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.cmbbxState);
+            this.Controls.Add(this.cmbbxType);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtMobileNumber);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblMobilenumber);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.lblRegisterUser);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRegisterUser;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblMobilenumber;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtMobileNumber;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.ComboBox cmbbxType;
+        private System.Windows.Forms.ComboBox cmbbxState;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.RadioButton rdbbtnMale;
+        private System.Windows.Forms.RadioButton rdbbtnFemale;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
